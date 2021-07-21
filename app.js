1ed9816bd377e76be89cdf67c1e7ebf3b39581e2
adamesalles <eadamesalles@gmail.com>
2021-07-21 18:35:43 -0300

    Adding comments | Removing Portuguese

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let count = 0;
 
 // Copy this part next
 while (a){
+    // Decides which way to walk.
     switch (count){
         case 0:
             left.click();
@@ -33,16 +34,19 @@ while (a){
             count = 0;
             break;
     }
+    // Waits 3 sec.
     await new Promise(r => setTimeout(r, 3000));
+    // Gets the pokémon element (if it exists), if not, then = null
     poke = document.getElementById('dexy');
     if (poke != null) {
-        console.log(poke.innerText);
+        console.log(poke.innerText); // Name of the pokémon
+        // Gets the star that shows it's a legendary (if it exists), if not, then = undefined
         star = document.getElementsByClassName("fas fa-star spicon spib")[0]
         if (star != undefined){
-            const c = document.querySelector('.btn-catch-action')
+            const c = document.querySelector('.btn-catch-action') // Catch button
             c.click()
-            a =  false;
-            alert('Você encontrou um lendário.')
+            a =  false; // Breaks the loop.
+            alert('You have found a Legendary Pokémon'); // Stops the script until hits 'ok'.
         } else {
         a = true;
     }
@@ -51,11 +55,13 @@ while (a){
 }
 //
 
-// Road to catch:
 
+// STAR
 // <i class="fas fa-star spicon spib" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="" data-original-title="This is considered a Legendary Pokemon"></i>
-
 // document.getElementsByClassName("fas fa-star")[0]
+
+
+// Road to catch:
 
 // <input class="btn-battle-action" type="submit" name="Start Battle" value="Start Battle">
 
